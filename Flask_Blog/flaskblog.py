bce54f2e89deaa73b83af91dc8659139ce67a2cf
Jesse <jesse.shea@qixingdesign.com>
2019-02-23 19:35:24 -0800

    finished 2nd video

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Dick Butkis',
+        'title': 'Fendi Booty',
+        'content': 'FENDI FENDI FENDI! BOOTY BOOTY BOOTY!',
+        'date_posted': 'April 1st, 2099'
+    },
+    {
+        'author': 'Assy McChapstick',
+        'title': 'Piddy pop on the tippy tai',
+        'content': 'Watotay mah damie? Seppy tow on the tippy tai.',
+        'date_posted': 'December 3rd, 2099'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
