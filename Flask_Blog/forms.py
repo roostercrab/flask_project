43f3d4beb7a04d3d7ff34fecd7313c6358054728
Jesse <jesse.shea@qixingdesign.com>
2019-02-24 13:44:21 -0800

    flask won't import flask_wtf

---
+++ Flask_Blog/forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    #could use EmailField?
+    email = StringField('Email', validators=[DataRequired(), Email()]) 
+
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    # Could use username for login
+    # username = StringField('Username', 
+    #     validators=[
+    #         DataRequired(), 
+    #         Length(min=2, max=20)
+    #         ]
+    #         )
+    #could use EmailField?
+    email = StringField('Email', validators=[DataRequired(), Email()]) 
+
+    password = PasswordField('Password', validators=[DataRequired()])
+    
+    remember = BooleanField('Remember Me')
+    
+    submit = SubmitField('Login')
